a1bbb724d420f81e5345fdeddeffdf4649b736d1
Rishav Rungta <rishav@utilize.app>
2020-05-27 18:18:49 +0530

    Upgrade to react app rewired
    Add eslint and dependencies. Remove useless files. Yarn upgrade.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+const App: React.FC = () => {
 	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.tsx</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-			</header>
+		<div>
+			<div>Show some love ❤️</div>
+			<div>
+				Support{' '}
+				<a href="https://github.com/rishav394/vit-faculty-search-react">this</a>{' '}
+				project with a star ⭐
+			</div>
 		</div>
 	);
-}
+};
 
 export default App;
